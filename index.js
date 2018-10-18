57954b3bc3c9afbf9b6fb23705bf41a69bc3ad8e
Fasnacht <joyf@amazon.com>
2018-10-17 23:18:50 -0400

    Adding more randomness into data, also modifying for different "batches" of data per quarter.

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ const temp_dir = './outputs';
 // clean out temp outputs directory
 rimraf.sync(temp_dir,{},()=>{console.log('deleted old temp outputs')} );
 
+function randomIntInc(low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low)
+}
+
 // object of configurable constants to set defaults in the output
 const conf = {
     city_name: "Las Vegas",
     state_code: "NV",
-    zipcode: "89102",
-    price: "12.99",
     truckid: 2,
     warehouse_street: "12 Business Park Court",
     warehouse_city: "Las Vegas",
@@ -32,7 +34,9 @@ const conf = {
     // order_year: "2018"
 }
 
-var prices = ['9.99', '12.49', '19.99', '24.99', '5.65', '32.49', '59.99', '45.00', '52.25'];
+var prices = ['9.99', '12.49', '19.99', '24.99', '5.65', '32.49', '59.99', '45.00', '52.25', '28.99'];
+
+var zip_codes = ["89179", "89109", "89138", "89143", "89166", "89144", "89146", "89118", "89145", "89120", "89134", "89141", "89178", "89139", "89130", "89128", "89183", "89148", "89131", "89101", "89147", "89129", "89123", "89121", "89110"];
 
 var id = 1; // global scope variable to track customer id
 var orderid = 1000; 
@@ -83,25 +87,27 @@ function transformAddressCsv(data){
 		, integer: true
 	});
         var orderDT = randomdate({
-                        year: 2014,
-                        month: 1
+                        year: 2015,
+                        month: 2
                 });
         var promisedDT = new Date(orderDT);
         let promDT = datetime.addDays(promisedDT, 1);
+        var randomZipInt = randomIntInc(0,21);
+ 	var randomPriceInt = randomIntInc(0,9);
         minimizedData = { // only copy lat, long and street address
 
  	    orderdate: datetime.format(orderDT,
                 'YYYY-MM-DDTHH:mm'),
-	    address: data.NUMBER +' '+ data.STREET.replace(/ +(?= )/g,''),
+	    address: data.number +' '+ data.street.replace(/ +(?= )/g,''),
 	    city: conf.city_name,
             state: conf.state_code,
-            zip: conf.zipcode,
-            lat: data.LAT,
-            lon: data.LON,
+            zip: zip_codes[randomZipInt],
+            lat: data.lat,
+            lon: data.lon,
             // regex to take out extra spaces inside string
 	    orderid: orderid++,
             customer_id: id++,
-            price: conf.price,
+            price: prices[randomPriceInt],
             truck: conf.truckid,
             warehouse_address: conf.warehouse_street,
             warehouse_city: conf.warehouse_city,
@@ -122,4 +128,4 @@ function transformAddressCsv(data){
 }
 
 // runs a test for local file
-genSampleAddresses('las_vegas_area_addresses.csv','addresses.csv');
+genSampleAddresses('../../Documents/Projects/addresses_q1_2015.csv', 'addresses_q1_2015_out.csv');
